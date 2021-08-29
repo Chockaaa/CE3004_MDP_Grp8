231a5bb34889075b179b88f0981bc064c229dbf9
chockaaa <chocka1998@gmail.com>
2021-08-29 21:19:58 +0800

    3

--- MDPGrp8/Release/Core/Src/oled.d
+++ MDPGrp8/Release/Core/Src/oled.d
@@ -23,6 +23,7 @@ Core/Src/oled.o: ../Core/Src/oled.c ../Core/Inc/oled.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h \
  ../Core/Inc/oledfont.h
 
 ../Core/Inc/oled.h:
@@ -74,5 +75,7 @@ Core/Src/oled.o: ../Core/Src/oled.c ../Core/Inc/oled.h \
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
 
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h:
 
 ../Core/Inc/oledfont.h:
