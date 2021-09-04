21da188310c603dde80ad19d41314e6eeee3869a
chockaaa <chocka1998@gmail.com>
2021-09-05 00:28:36 +0800

    uart communication 2 char

--- MDPGrp8/UART.py
+++ MDPGrp8/UART.py
@@ -1,17 +1,15 @@
 port='/dev/ttyS0'
 Baudrate=115200
 import serial
-x=100020003000
-y='a'
+y="11"
 ser=serial.Serial('COM4', baudrate=Baudrate,timeout=1)
 while 1:
     
-    data=ser.readline().decode('ascii')
     
-    y=str(x)
+    data=ser.readline().decode('ascii')
     ser.write(bytes(y, 'UTF-8'))
     print(data)
-    x=x+1
+    
 
 
 
