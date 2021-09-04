17bcf7e499661ce1a5dd2b9fd9da129f46fbfcb6
chockaaa <chocka1998@gmail.com>
2021-09-05 01:34:16 +0800

    servo motor code

--- MDPGrp8/UART.py
+++ MDPGrp8/UART.py
@@ -1,14 +1,15 @@
 port='/dev/ttyS0'
 Baudrate=115200
 import serial
-y="10"
+listt=["10","20","11","21","12","22"]
+y="20"
 ser=serial.Serial('COM4', baudrate=Baudrate,timeout=1)
 i=0
 while 1:
     i=i+1
     
     data=ser.readline().decode('ascii')
-    ser.write(bytes(y, 'UTF-8'))
+    ser.write(bytes(listt[i%6], 'UTF-8'))
     print(data)
     
     
