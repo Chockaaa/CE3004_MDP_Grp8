15456ec17a14dda5cd0703aca24280232e8385f7
chockaaa <chocka1998@gmail.com>
2021-09-15 01:29:28 +0800

    turning

--- MDPGrp8/UART.py
+++ MDPGrp8/UART.py
@@ -3,7 +3,7 @@ Baudrate=115200
 import serial
 listt=["10","20","11","21","12","22"]
 y="20"
-ser=serial.Serial('COM7', baudrate=Baudrate,timeout=1)
+ser=serial.Serial('COM5', baudrate=Baudrate,timeout=1)
 i=0
 while 1:
     i=i+1
