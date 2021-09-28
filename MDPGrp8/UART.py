c03e8c7b0b1e56be06df98b2cbe2018db149b651
chockaaa <chocka1998@gmail.com>
2021-09-28 21:47:51 +0800

    sample

--- MDPGrp8/UART.py
+++ MDPGrp8/UART.py
@@ -2,20 +2,22 @@ port='/dev/ttyS0'
 Baudrate=115200
 import serial
 import time
-listt=["10","20","11","21","12","22"]
-y="20"
+
 ser=serial.Serial('COM5', baudrate=Baudrate,timeout=1)
 i=0
+data=""
 while 1:
-    i=i+1
+   
     
     
-    ser.write(bytes(input(), 'UTF-8'))
-    time.sleep(1)
-    data=ser.readline().decode('UTF-8')
-    print(data)
+    ser.write(bytes(input("Input:"), 'UTF-8'))
+    time.sleep(0.5)
     ser.write(bytes('00000', 'UTF-8'))
-    
+    print("Output:")
+    while(data!="READY"):
+        data=ser.readline().decode('UTF-8')
+        print(data)
+    data=""    
     
     
 
